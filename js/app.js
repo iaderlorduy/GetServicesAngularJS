787fa3690a383bfa7743454ccaeea99a7f492a49
iaderlorduy <iaderlor@gmail.com>
2015-09-22 11:32:02 -0500

    Implementación

---
+++ js/app.js
@@ -0,0 +1,31 @@
+//para hacer uso de $resource debemos colocarlo al crear el modulo
+var app = angular.module("app", ["ngResource"]);
+
+//con dataResource inyectamos la factoría
+app.controller("appController", function ($scope, $http, dataResource) {
+    //hacemos uso de $http para obtener los datos del json
+    $http.get('http://jloracudris.azurewebsites.net/API/Artistas').success(
+
+      function (data) {
+        //Convert data to array.
+        //datos lo tenemos disponible en la vista gracias a $scope
+        $scope.datos = data;
+        console.log('data');
+        console.log(data);
+      }
+    );
+    //datosResource lo tenemos disponible en la vista gracias a $scope
+    $scope.datosResource = dataResource.get();
+
+
+})
+
+//de esta forma tan sencilla consumimos con $resource en AngularJS
+app.factory("dataResource", function ($resource) {
+    return $resource("http://jloracudris.azurewebsites.net/API/Artistas", //la url donde queremos consumir
+        {}, //aquí podemos pasar variables que queramos pasar a la consulta
+        //a la función get le decimos el método, y, si es un array lo que devuelve
+        //ponemos isArray en true
+        { get: { method: "GET", isArray: true }
+    })
+})
